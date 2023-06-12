60e89b2b396328d33a6c1de0a74df5875b37e358
guoyebin <982317078@qq.com>
2023-06-12 16:05:35 +0800

    Initial commit

---
+++ src/main/java/com/guo/javalearncode/JavaLearnCodeApplication.java
@@ -0,0 +1,13 @@
+package com.guo.javalearncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearnCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLearnCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guo/javalearncode/JavaLearnCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guo.javalearncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearnCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
